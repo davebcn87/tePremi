eff1ebc177c57f49de65291645d91a142878b8e0
Xavi <xavi1987@gmail.com>
2012-06-19 15:31:15 +0200

    Done Avantatges View

--- TePremi/TePremi/instruccionsViewController.h
+++ TePremi/TePremi/instruccionsViewController.h
@@ -10,4 +10,6 @@
 
 @interface instruccionsViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UIScrollView *iSroll;
+
 @end
--- TePremi/TePremi/instruccionsViewController.m
+++ TePremi/TePremi/instruccionsViewController.m
@@ -14,6 +14,8 @@
 
 @implementation instruccionsViewController
 
+@synthesize iSroll;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -27,6 +29,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    [iSroll setContentSize:CGSizeMake(320, 774)];
 }
 
 - (void)viewDidUnload
